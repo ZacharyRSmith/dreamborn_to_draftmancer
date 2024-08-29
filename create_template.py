b525fa4e003220f5d1528d8d440af9e3a90cd9b1
Dru Erridge <druerridge@users.noreply.github.com>
2024-08-28 22:49:23 -0700

    Got card evaluations working and reworked a bunch of stuff. Just need a cleanup pass for readability, then I can add the other features.

--- create_template.py
+++ create_template.py
@@ -18,6 +18,7 @@ from collections import defaultdict
 import json
 from pathlib import Path
 import re
+import csv
 
 API_DATA_FILEPATH = 'api_data.json'
 OUT_FILEPATH = 'draftmancer_custom_card_list_template.txt'
@@ -47,21 +48,26 @@ def fetch_api_data():
     return name_to_card
 
 
-def read_name_to_card():
+def generate_name_id_to_card(name_to_card):
+    return {to_id(card_name) : name_to_card[card_name] for card_name in name_to_card}
+
+def read_or_fetch_name_id_to_api_card():
     api_data_file = Path(API_DATA_FILEPATH)
     if api_data_file.is_file():
         with api_data_file.open() as f:
-            name_to_card = json.load(f)
+            name_id_to_card = json.load(f)
     else:
         name_to_card = fetch_api_data()
+        fix_card_names(name_to_card)
+        name_id_to_card = generate_name_id_to_card(name_to_card)
         with api_data_file.open(mode='w') as f:
-            json.dump(name_to_card, f)
-    return name_to_card
+            json.dump(name_id_to_card, f)
+    return name_id_to_card
 
 
 def read_id_to_vals(dreamborn_export_for_tabletop_sim__filepath):
     dreamborn_export_for_tabletop_sim__file = Path(dreamborn_export_for_tabletop_sim__filepath)
-    with dreamborn_export_for_tabletop_sim__file.open() as f:
+    with dreamborn_export_for_tabletop_sim__file.open(encoding='utf8') as f:
         data = json.load(f)
     data = data['ObjectStates'][0]
 
@@ -82,27 +88,30 @@ def read_id_to_vals(dreamborn_export_for_tabletop_sim__filepath):
     return id_to_vals
 
 
-pattern = re.compile('[\W_]+', re.ASCII)
+pattern = re.compile(r"[\W_]+", re.ASCII)
 def to_id(string):
     string = string.replace('ā', 'a')
     return re.sub(pattern, '', string).lower()
 
 
-def transform_name_to_card(name_to_card):
+def fix_card_name(name_to_card, old_name, new_name):
+    name_to_card[new_name] = name_to_card[old_name]
+    name_to_card[new_name]['Name'] = new_name
+    del name_to_card[old_name]    
+
+def fix_card_names(name_to_card):
     # set correct keys using API's typos
-    name_to_card['Rabbit - Reluctant Host'] = name_to_card['Rabbit - Reluctent Host']
-    name_to_card['Pinocchio - Talkative Puppet'] = name_to_card['Pinocchio - Talkative Pupper']
-    name_to_card['Perplexing Signposts'] = name_to_card['Preplexing Signposts']
-    name_to_card['Kristoff - Official Ice Master'] = name_to_card['Kristoff - Offical Ice Master']
-
-    # set ids using names
-    for name, card in list(name_to_card.items()):
-        id = to_id(name)
-        name_to_card[id] = card
-
+    fix_card_name(name_to_card, 'Benja - Bold United', 'Benja - Bold Uniter')
+    fix_card_name(name_to_card, 'Kristoff - Offical Ice Master', 'Kristoff - Official Ice Master')
+    fix_card_name(name_to_card, 'Snowanna Rainbeau', 'Snowanna Rainbeau - Cool Competitor')
+    fix_card_name(name_to_card, 'Vannelope Von Schweetz - Random Roster Racer', 'Vanellope von Schweetz - Random Roster Racer')
+    fix_card_name(name_to_card, 'Snow White - Fair-haired', 'Snow White - Fair-Hearted')
+    fix_card_name(name_to_card, 'Merlin\'s Cottage', 'Merlin\'s Cottage - The Wizard\'s Home')
+    fix_card_name(name_to_card, 'Arthur - King Victorius', 'Arthur - King Victorious')
+    fix_card_name(name_to_card, 'Seven Dwarfs\' Mine', 'Seven Dwarfs\' Mine - Secure Fortress')
 
 def read_all_card_names_from_dreamborn_plain_export():
-    with open('all_cards.txt') as f:
+    with open('all_cards.txt', encoding='utf8') as f:
         lines = f.readlines()
     id_to_name = {}
     for l in lines:
@@ -111,35 +120,39 @@ def read_all_card_names_from_dreamborn_plain_export():
     return id_to_name
 
 
-def get_out(id_to_vals, name_to_card):
-    out = []
-    id_to_name_from_plain_export = read_all_card_names_from_dreamborn_plain_export()
-    for id, vals in id_to_vals.items():
-        if id in name_to_card:
-            ink_cost = name_to_card[id]['Cost']
-        else:
-            print(f'Ink cost not found for: {vals["name"]}')
-            ink_cost = 0
-
-        if to_id(vals['name']) in id_to_name_from_plain_export:
-            name = id_to_name_from_plain_export[to_id(vals['name'])]
-        else:
-            print(f"Name {vals['name']} from dreamborn TableTop Simulator export not found in names from dreamborn plain export!")
-            name = vals['name']
-
-        out.append({
-            'name': name,
+def generate_custom_card_list(name_to_card, name_to_rating, id_to_vals):
+    custom_card_list = []
+    # id_to_name_from_plain_export = read_all_ca_names_from_dreamborn_plain_export()
+    for name_id in id_to_vals:
+        ink_cost = name_to_card[name_id]['Cost']
+        set_num = name_to_card[name_id]['Set_Num']
+        card_num = name_to_card[name_id]['Card_Num']
+        foo = {
+            'name': id_to_vals[name_id]['name'],
             'mana_cost': f'{{{ink_cost}}}',
             'type': 'Instant',
             'image_uris': {
-                'en': vals['image_uri'],
+                'en': f"https://images.dreamborn.ink/cards/en/tts/00{set_num}-{'{:03}'.format(card_num)}_716x1000.jpeg",
             },
-        })
-    return out
+        }
+        # foo['rarity'] = lorcana_rarity_to_magic_rarity(name_to_card['Rarity'])
+        foo['rating'] = name_to_rating[name_id]
+        custom_card_list.append(foo)
+    return custom_card_list
 
+def retrieve_name_id_to_rating():
+    name_id_to_rating = {}
+    with open(file="DraftBots\\FrankKarstenDraftBotEvaluations.csv", newline='', encoding='utf8') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        dialect.quoting = csv.QUOTE_MINIMAL
+        csvfile.seek(0)
+        reader = csv.DictReader(csvfile, dialect=dialect)
+        for row in reader:
+            name_id_to_rating[to_id(row['Card Name'])] = int(row['Rating - Draftmancer'])
+    return name_id_to_rating
 
-def write_out(out):
-    with open(OUT_FILEPATH, 'w') as f:
+def write_out(out, id_to_vals):
+    with open(OUT_FILEPATH, 'w', encoding="utf-8") as f:
         lines = [
             '[CustomCards]',
             json.dumps(out, indent=4),
@@ -152,15 +165,20 @@ def write_out(out):
             ),
             f'[MainSlot(12)]',
         ]
+        for id in id_to_vals:
+            line_str = f"{id_to_vals[id]['count']} {id_to_vals[id]['name']}"
+            lines.append(line_str)
+
         for line in lines:
             f.write(line + '\n')
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-
     id_to_vals = read_id_to_vals(args.dreamborn_export_for_tabletop_sim)
-    name_to_card = read_name_to_card()
-    transform_name_to_card(name_to_card)
-    out = get_out(id_to_vals, name_to_card)
-    write_out(out)
+    # print(id_to_vals)
+    name_id_to_api_card = read_or_fetch_name_id_to_api_card()
+    name_id_to_rating = retrieve_name_id_to_rating()
+    custom_card_list = generate_custom_card_list(name_id_to_api_card, name_id_to_rating, id_to_vals)
+    write_out(custom_card_list, id_to_vals)
+    # print()
